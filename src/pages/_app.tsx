01e536315b424a05a4ff62cb83a713c500f92bbb
mengxue xu <mxu4321@gmail.com>
2023-04-17 11:41:14 -0700

    added bg img into _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,8 +7,11 @@ import Resume from "@/pages/resume";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import Image from "next/image";
+import bgImg from "../assets/images/bg.jpg"
 
 export default function App({ Component, pageProps }: AppProps) {
+  // console.log(bgImg);
   return (
     <div>
       <Head>
@@ -17,7 +20,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main id="mainTag" className="bg-slate-800 min-h-screen text-slate-100">
+      <main id="mainTag" className="bg-slate-800 min-h-screen text-slate-100" style={{backgroundImage: `url(${bgImg.src})`}}>
         {/* ------ ❄️ nav bar ------- */}
         <NavBar />
 
